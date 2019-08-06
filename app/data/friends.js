690c2bfb39450377e127a12651415f5f4199101d
sinid <49916315+oinksu@users.noreply.github.com>
2019-08-06 17:05:21 -0400

    the whole kitten kabuttle

---
+++ app/data/friends.js
@@ -0,0 +1,56 @@
+// array that holds all the 'friends' that have taken the survey
+// initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Penny The Dog',
+    profilePic: 'https://photos.app.goo.gl/yhrgy4xUMXb5smMQA',
+    scores: [
+      '2',
+      '4',
+      '5',
+      '2',
+      '4',
+      '5',
+      '4',
+      '4',
+      '3',
+      '4'
+    ]
+  },
+
+  {
+    name: 'Scarlett The Dog',
+    profilePic: 'https://photos.app.goo.gl/32orV67nHyb1VQt38',
+    scores: [
+      '2',
+      '4',
+      '1',
+      '2',
+      '4',
+      '5',
+      '4',
+      '5',
+      '3',
+      '4'
+    ]
+  },
+
+  {
+    name: 'Princess Leia',
+    photo: 'http://images.techtimes.com/data/images/full/168082/princess-leia-organa.jpg?w=600',
+    scores: [
+      '5',
+      '3',
+      '5',
+      '4',
+      '5',
+      '3',
+      '1',
+      '3',
+      '2',
+      '4'
+    ]
+  }
+]
+
+module.exports = friendList
--- server.js
+++ server.js
@@ -1,23 +1,36 @@
+// Node Dependencies
 var express = require('express')
 var bodyParser = require('body-parser')
 var path = require('path')
 
+
+
+// Link in html and api routes
+var apiRoutes = require('./app/routing/api-routes.js')
+var htmlRoutes = require('./app/routing/html-routes.js')
+
+
+
+// Set up Express App
 var app = express()
+var PORT = process.env.PORT || 3100
 
-var PORT = process.env.PORT || 3000
 
-// For serving of static CSS
-app.use(express.static(__dirname + '/app/css'))
 
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.text())
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 
-// API and HTML routes
-require('./app/routing/apiRoutes.js')(app)
-require('./app/routing/htmlRoutes.js')(app)
 
+
+// Server Routing Map 
+apiRoutes(app) // API route - Must be listed first due to the HTML default catch all "use" route
+htmlRoutes(app) // HTML route 
+
+
+// Listener - Start the server
 app.listen(PORT, function () {
   console.log('App listening on PORT: ' + PORT)
 })
